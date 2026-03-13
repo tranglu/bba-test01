f6404b6377566b0dcd41aa89e71b50d9b6f10035
test 01 - Trang Lữ <lu.t.t.trang@gmail.com>
2026-03-13 23:34:08 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,10 @@
+let playerName = "Mario";
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3=45;
+
+let tong = level1+level2+level3;
+let TB = tong/3;
+let soDu = tong%3;
+console.log(soDu);
