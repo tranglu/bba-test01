81df24d7594df01260a8c0e7acc232915934ab81
test 01 - Trang Lữ <lu.t.t.trang@gmail.com>
2026-03-16 10:58:55 +0700

    feat/ change variable type

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -4,7 +4,7 @@ const level1 = 25;
 const level2 = 30;
 const level3 = 45;
 
-let sum = level1 + level2 + level3;
-let average = sum / 3;
-let remainder = sum % 3;
+const sum = level1 + level2 + level3;
+const average = sum / 3;
+const remainder = sum % 3;
 console.log(remainder);
