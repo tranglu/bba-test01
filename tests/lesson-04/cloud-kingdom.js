2814076a7228be1f9cca4f294793552670c2d0d5
test 01 - Trang Lữ <lu.t.t.trang@gmail.com>
2026-03-16 10:57:05 +0700

    feat/format code

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -1,17 +1,17 @@
 let powerUp = "mushroom";
-let effect='';
+let effect = '';
 
-if(powerUp==="mushroom"){
-    effect="Mario becomes Super!";
-}else if( powerUp==="flower"){
-    effect="Mario can shoot fireballs!";
-}else if(powerUp==="star"){
-    effect="Mario is invincible!";
-}else if(powerUp==="none"){
-    effect="Mario is normal";
+if (powerUp === "mushroom") {
+    effect = "Mario becomes Super!";
+} else if (powerUp === "flower") {
+    effect = "Mario can shoot fireballs!";
+} else if (powerUp === "star") {
+    effect = "Mario is invincible!";
+} else if (powerUp === "none") {
+    effect = "Mario is normal";
 }
-else{
-    effect="Unknown power-up";
+else {
+    effect = "Unknown power-up";
 }
 
 console.log(effect);
