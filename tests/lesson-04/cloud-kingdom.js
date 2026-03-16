ca36121b07bd56187508e5d665385611c9b9e445
test 01 - Trang Lữ <lu.t.t.trang@gmail.com>
2026-03-16 10:55:40 +0700

    feat/ change variable name

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -2,9 +2,9 @@ let playerName = "Mario";
 let currentLives = 3;
 const level1 = 25;
 const level2 = 30;
-const level3=45;
+const level3 = 45;
 
-let tong = level1+level2+level3;
-let TB = tong/3;
-let soDu = tong%3;
-console.log(soDu);
+let sum = level1 + level2 + level3;
+let average = sum / 3;
+let remainder = sum % 3;
+console.log(remainder);
